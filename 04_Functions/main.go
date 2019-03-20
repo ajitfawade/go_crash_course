9068422aa77901c4866a1c8fc5142a46f6eb4c4f
Ajit Fawade <ajitfawade@gmail.com>
2019-03-20 10:15:06 +0530

    Functions in GO.

---
+++ 04_Functions/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greeting(name string) string {
+	return "Hello " + name
+}
+
+func getSum(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func main() {
+	fmt.Println(greeting("Ajit"))
+	fmt.Println(getSum(2, 2))
+}
