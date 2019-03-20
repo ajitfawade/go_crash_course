d24ec2f3572feec355282104ba935aabd6146635
Ajit Fawade <ajitfawade@gmail.com>
2019-03-20 10:13:53 +0530

    variables and data types in GO.

---
+++ 02_vars/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// MAIN TYPES
+	// string
+	// bool
+	// int
+	// int int8 int16 int32 int64
+	// uint uint8 uint16 uint32 uint64 uintptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64
+	// complex64 complex128
+
+	// var name = "Ajit"
+	// short hand
+	name, email := "Ajit", "ajit@gmail.com"
+	var age int32 = 30
+	const isCool = true
+	var size float32 = 1.3
+	// size := 1.3
+	fmt.Println(name, age, email)
+	fmt.Printf("%T\n", size)
+}
